59fe71da5d1419fde1c8f3558afb08648bc63262
ryuichi <ryuichi.1479xxx@gmail.com>
2025-11-16 12:14:16 +0900

    ESLint の設定変更を削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,15 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
-    rules: {
-      "@typescript-eslint/consistent-type-imports": [
-        "error",
-        { prefer: "type-imports" },
-      ],
-    },
-  },
 ];
 
 export default eslintConfig;
