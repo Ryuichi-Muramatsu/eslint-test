3c6172d04343dd73665d64e53d2f9a0f2464d6a0
ryuichi <ryuichi.1479xxx@gmail.com>
2025-11-18 22:01:18 +0900

    test_commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ const Page: FC = () => {
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello World</h1>
+          <h1 className="text-2xl font-bold">Hello Test</h1>
         </div>
       </div>
     </main>
